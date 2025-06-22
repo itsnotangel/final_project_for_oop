3135cab00daee149b0a693b8196125d05dfb6647
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 07:30:49 +0800

    Add dark blue background to game window.

--- tetris_game_oop/main.py
+++ tetris_game_oop/main.py
@@ -1,6 +1,7 @@
 import pygame
 
 pygame.init()
+dark_blue = (44, 44, 127)
 
 screen = pygame.display.set_mode((300, 600))
 pygame.display.set_caption("Python Tetris")
@@ -12,6 +13,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+        
+        screen.fill(dark_blue)
         pygame.display.update()
         clock.tick(60)
