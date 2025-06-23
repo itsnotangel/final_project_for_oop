9d8e6304eae2f4e0214903502c707d8321aa98c9
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-24 04:32:21 +0800

    Add score tracking and on-screen display below game board.

--- snake_game_oop/snake.py
+++ snake_game_oop/snake.py
@@ -4,6 +4,7 @@ from pygame.math import Vector2
 pygame.init()
 
 title_font = pygame.font.Font(None, 60)
+score_font = pygame.font.Font(None, 40)
 
 GREEN = (173, 204, 96)
 DARK_GREEN = (43, 51, 24)
@@ -59,6 +60,7 @@ class Game:
         self.snake = Snake()
         self.food = Food(self.snake.body)
         self.state = "RUNNING"
+        self.score = 0
 
     def draw(self):
         self.food.draw()
@@ -75,6 +77,7 @@ class Game:
         if self.snake.body[0] == self.food.position:
             self.food.position = self.food.generate_random_pos(self.snake.body)
             self.snake.add_segment = True
+            self.score += 1
     
     def check_collision_with_edges(self):
         if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1:
@@ -86,6 +89,7 @@ class Game:
         self.snake.reset()
         self.food.position = self.food.generate_random_pos(self.snake.body)
         self.state = "STOPPED"
+        self.score = 0 
         
     def check_collision_with_tail(self):
         headless_body = self.snake.body[1:]
@@ -129,7 +133,9 @@ while True:
 		(OFFSET-5, OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10), 5)
     game.draw()
     title_surface = title_font.render("Retro Snake", True, DARK_GREEN)
+    score_surface = score_font.render(str(game.score), True, DARK_GREEN)
     screen.blit(title_surface, (OFFSET-5, 20))
+    screen.blit(score_surface, (OFFSET-5, OFFSET + cell_size*number_of_cells +10))
 
     pygame.display.update()
     clock.tick(60)
