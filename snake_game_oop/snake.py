151a3d0d886cd4d7c8145909756fa016110c4d31
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-24 03:56:10 +0800

    Implemented snake growth mechanic by adding a segment when food is consumed.

--- snake_game_oop/snake.py
+++ snake_game_oop/snake.py
@@ -29,18 +29,22 @@ class Food:
         return position
 
 class Snake:
-	def __init__(self):
-		self.body = [Vector2(6, 9), Vector2(5,9), Vector2(4,9)]
-		self.direction = Vector2(1, 0)
-          
-	def draw(self):
-		for segment in self.body:
-			segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
-			pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)
-
-	def update(self):
-		self.body = self.body[:-1]
-		self.body.insert(0, self.body[0] + self.direction)
+    def __init__(self):
+        self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
+        self.direction = Vector2(1, 0)
+        self.add_segment = False
+        
+    def draw(self):
+        for segment in self.body:
+            segment_rect = (segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7)
+            
+    def update(self):
+        self.body.insert(0, self.body[0] + self.direction)
+        if self.add_segment == True:
+            self.add_segment = False
+        else:
+            self.body = self.body[:-1]
 
 class Game:
     def __init__(self):
@@ -58,6 +62,7 @@ class Game:
     def check_collision_with_food(self):
         if self.snake.body[0] == self.food.position:
             self.food.position = self.food.generate_random_pos(self.snake.body)
+            self.snake.add_segment = True
 
 screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
 
