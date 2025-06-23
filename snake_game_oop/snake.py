e8a074f4ef53784d4b306a214f69f8cad8c4487b
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-24 00:47:52 +0800

    Added main game loop that handles window events, updates the display, and limits the frame rate to 60 FPS using pygame.time.Clock.

--- snake_game_oop/snake.py
+++ snake_game_oop/snake.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 
 pygame.init()
 
@@ -7,3 +7,12 @@ screen = pygame.display.set_mode((750, 750))
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
