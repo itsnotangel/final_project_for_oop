afeed8361e2b74e0877f2b62faec0bc6ec53d149
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 09:19:54 +0800

    Imported Colors and assigned color list to Block class using get_cell_colors.

--- tetris_game_oop/blocks_for_tetris.py
+++ tetris_game_oop/blocks_for_tetris.py
@@ -1,6 +1,9 @@
+from colors_for_tetris import Colors
+
 class Block:
     def __init__(self, id):
         self.id = id
         self.cells = {}
         self.cell_size = 30
         self.rotation_state = 0    
+        self.colors = Colors.get_cell_colors()
