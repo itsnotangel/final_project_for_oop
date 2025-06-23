20c26c6ed0eff8734e8f2e8882d232ef3eb31466
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 09:37:46 +0800

    Added draw method to Block class to render current rotation state using pygame rectangles

--- tetris_game_oop/blocks_for_tetris.py
+++ tetris_game_oop/blocks_for_tetris.py
@@ -1,4 +1,5 @@
 from colors_for_tetris import Colors
+import pygame
 
 class Block:
     def __init__(self, id):
@@ -7,3 +8,10 @@ class Block:
         self.cell_size = 30
         self.rotation_state = 0    
         self.colors = Colors.get_cell_colors()
+
+    def draw(self, screen):
+        tiles = self.cells[self.rotation_state]
+        for tile in tiles:
+            tile_rect = pygame.Rect(tile.column * self.cell_size + 1, tile.row * self.cell_size + 1,
+            self.cell_size -1, self.cell_size -1)
+            pygame.draw.rect(screen, self.colors[self.id], tile_rect)
