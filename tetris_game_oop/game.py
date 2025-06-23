5adac9cbae1d4215f272e6769bb1145b79ab4c5b
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 17:18:19 +0800

    Add block_fits check to prevent overlapping during downward movement.

--- tetris_game_oop/game.py
+++ tetris_game_oop/game.py
@@ -28,7 +28,7 @@ class Game:
 
     def move_down(self):
         self.current_block.move(1, 0)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(-1, 0)
             self.lock_block()
 
@@ -38,6 +38,13 @@ class Game:
             self.grid.grid[position.row][position.column] = self.current_block.id
         self.current_block = self.next_block
         self.next_block = self.get_random_block()
+
+    def block_fits(self):
+        tiles = self.current_block.get_cell_positions()
+        for tile in tiles:
+            if self.grid.is_empty(tile.row, tile.column) == False:
+                return False
+        return True
             
     def rotate(self):
         self.current_block.rotate()
