dbb411b9ae6240ebe2775f7cddd318b0fba610c7
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 11:11:07 +0800

    Add Game class to manage grid, handle current and next block logic, and draw them to the screen.

---
+++ tetris_game_oop/game.py
@@ -0,0 +1,21 @@
+from grid_for_tetris import Grid
+from individual_blocks import *
+import random
+
+class Game:
+	def __init__(self):
+		self.grid = Grid()
+		self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		self.current_block = self.get_random_block()
+		self.next_block = self.get_random_block()
+
+	def get_random_block(self):
+		if len(self.blocks) == 0:
+			self.blocks = [IBlock(), JBlock(), LBlock(), OBlock(), SBlock(), TBlock(), ZBlock()]
+		block = random.choice(self.blocks)
+		self.blocks.remove(block)
+		return block
+	
+	def draw(self, screen):
+		self.grid.draw(screen)
+		self.current_block.draw(screen)
