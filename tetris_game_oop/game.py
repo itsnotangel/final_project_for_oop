6323e502bb7ea0a3c2a33ae82a34c59355f019f5
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 17:21:05 +0800

    Add block_fits validation to all block movements to prevent overlapping.

--- tetris_game_oop/game.py
+++ tetris_game_oop/game.py
@@ -18,12 +18,12 @@ class Game:
 
     def move_left(self):
         self.current_block.move(0, -1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(0, 1)
 
     def move_right(self):
         self.current_block.move(0, 1)
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.move(0, -1)
 
     def move_down(self):
@@ -48,7 +48,7 @@ class Game:
             
     def rotate(self):
         self.current_block.rotate()
-        if self.block_inside() == False:
+        if self.block_inside() == False or self.block_fits() == False:
             self.current_block.undo_rotation()
 
     def block_inside(self):
