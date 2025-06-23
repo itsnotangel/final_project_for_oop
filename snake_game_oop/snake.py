362efb97c37e7ad30724447a5a47f3b7ee747877
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-24 01:05:01 +0800

    Added a Food class with a fixed position and implemented a draw method to render the food block on the game grid.

--- snake_game_oop/snake.py
+++ snake_game_oop/snake.py
@@ -1,15 +1,29 @@
 import pygame, sys
+from pygame.math import Vector2
 
 pygame.init()
 
 GREEN = (173, 204, 96)
 DARK_GREEN = (43, 51, 24)
 
-screen = pygame.display.set_mode((550, 550))
+cell_size = 30
+number_of_cells = 25
+
+class Food:
+    def __init__(self):
+        self.position = Vector2(5, 6)
+        
+    def draw(self):
+        food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, DARK_GREEN, food_rect)
+
+screen = pygame.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells))
 
 pygame.display.set_caption("Retro Snake")
 
 clock = pygame.time.Clock()
+
+food = Food()
 
 while True:
     for event in pygame.event.get():
@@ -18,5 +32,7 @@ while True:
             sys.exit()
 
     screen.fill(GREEN)
+    food.draw()
+
     pygame.display.update()
     clock.tick(60)
