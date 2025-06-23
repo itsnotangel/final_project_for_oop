8f6749b76c0c1342531be85c6dff33f755e8d1d9
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 09:30:15 +0800

    Add LBlock subclass with rotation states and initial horizontal offset.

---
+++ tetris_game_oop/individual_blocks.py
@@ -0,0 +1,13 @@
+from blocks_for_tetris import Block
+from position_for_tetris import Position
+
+class LBlock(Block):
+	def __init__(self):
+		super().__init__(id = 1)
+		self.cells = {
+			0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
+			1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
+			2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
+			3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
+		}
+		self.move(0, 3)
