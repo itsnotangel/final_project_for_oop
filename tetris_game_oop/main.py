721462fca9d1ec601f74ddcc66b8fbf3af2b91fd
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-23 11:46:10 +0800

    Add left arrow key support to move current block left.

--- tetris_game_oop/main.py
+++ tetris_game_oop/main.py
@@ -16,6 +16,9 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_LEFT:
         
         screen.fill(dark_blue)
         game.draw(screen)
