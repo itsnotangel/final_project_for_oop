798e6522df8e749fa462e63aafa04693854acacf
itsnotangel <angellaikaclarito04@gmail.com>
2025-06-24 09:10:50 +0800

    Fixed a bug in rotate by replacing '==' with '=' to correctly reset the rotation state, and updated undo_rotation to handle negative values properly.

--- tetris_game_oop/blocks_for_tetris.py
+++ tetris_game_oop/blocks_for_tetris.py
@@ -27,11 +27,11 @@ class Block:
     def rotate(self):
         self.rotation_state += 1
         if self.rotation_state == len(self.cells):
-            self.rotation_state == 0
+            self.rotation_state = 0
             
     def undo_rotation(self):
         self.rotation_state -= 1
-        if self.rotation_state == 0:
+        if self.rotation_state < 0:
             self.rotation_state = len(self.cells) - 1
 
     def draw(self, screen, offset_x, offset_y):
